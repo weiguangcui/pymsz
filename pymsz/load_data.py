a381b6f0c9513dcb5a106f7a062593abe227e9e8
weiguangcui <cuiweiguang@gmail.com>
2017-05-25 15:19:05 +0200

    add center radius to the data class

--- pymsz/load_data.py
+++ pymsz/load_data.py
@@ -11,15 +11,25 @@ class load_data(object):
     Parameters
     ----------
     filename    : The filename of simulation snapshot, or data. Default : ''
-    snapshot    : Is loading snapshot or not? Default : True
+    snapshot    : Is loading snapshot or not? Default : False
     metal       : Gas metallicity. Default: None, will try to read from simulation.
                   Otherwise, will use this give metallicity, which can be float,
                   or array (Must be the same number of gas particles)
 
     yt_load     : Do you want to use yt to load the data? Default : False. Requries yt modelds.
 
-    specified_field: If you want to specify the data fields for yt.load. Default: None.
-                    Only works with yt_load = True.
+    specified_field: If you want to specify the data fields (for Gadget snapshots) for yt.load.
+                    Default: None. Only works with yt_load = True. For example:
+                    My_def = ("Coordinates", "Velocities", "ParticleIDs", "Mass",
+                              ("InternalEnergy", "Gas"),
+                              ("Density", "Gas"),
+                              ("ElectronAbundance", "Gas"),
+                              ("NeutralHydrogenAbundance", "Gas"),
+                              ("SmoothingLength", "Gas"),
+                              ("StarFomationRate", "Gas"),
+                              ("Age", ("Stars", "Bndry")),
+                              ("Z", ("Gas","Stars")),
+                             )
 
     rawdata     : Is it raw data? Default : False
                   Please look at (or change) the reading function in this file to load the raw data.
@@ -44,9 +54,12 @@ class load_data(object):
 
     """
 
-    def __init__(self, filename='', snapshot=True, metal=None, yt_load=False,
+    def __init__(self, filename='', snapshot=False, metal=None, yt_load=False,
                  specified_field=None, datafile=False, center=None, radius=None):
 
+        self.center = center
+        self.radius = radius
+        self.filename = filename
         if snapshot:
             self.data_type = "snapshot"
             self.temp = np.array([])
@@ -67,18 +80,18 @@ class load_data(object):
             # self.Uage = 0.0  # university age in Gyrs
             # self.nx = self.grid_mass = self.grid_age = self.grid_metal = None
 
-            self._load_snap(filename, center, radius)
+            self._load_snap()
         elif yt_load:
             self.data_type = "yt_data"
-            self.data = self._load_yt(filename, center, radius, specified_field)
-        elif datafile:
-            self.data_type = "snapshot"
-            self._load_raw(datafile, center, radius)
+            self.data = self._load_yt(specified_field)
+        # elif datafile:
+        #     self.data_type = "snapshot"
+        #     self._load_raw()
         else:
             raise ValueError("Please sepecify the simulation data type. ")
 
-    def _load_snap(self, filename, cc, rr):
-        head = readsnapsgl(filename, "HEAD", quiet=True)
+    def _load_snap(self):
+        head = readsnapsgl(self.filename, "HEAD", quiet=True)
         self.cosmology["z"] = head[3] if head[3] > 0 else 0.0
         self.cosmology["a"] = head[2]
         self.cosmology["omega_matter"] = head[-3]
@@ -90,41 +103,41 @@ class load_data(object):
         # self.z = head[3] if head[3] > 0 else 0.0
 
         # positions # only gas particles
-        spos = readsnapsgl(filename, "POS ", ptype=0, quiet=True)
-        if (cc is not None) and (rr is not None):
-            r = np.sqrt(np.sum((spos - cc)**2, axis=1))
-            ids = r <= rr
-            self.pos = spos[ids] - cc
+        spos = readsnapsgl(self.filename, "POS ", ptype=0, quiet=True)
+        if (self.center is not None) and (self.radius is not None):
+            r = np.sqrt(np.sum((spos - self.center)**2, axis=1))
+            ids = r <= self.radius
+            self.pos = spos[ids] - self.center
         else:
             ids = np.ones(head[0][0], dtype=bool)
             self.pos = spos - np.mean(spos, axis=0)
 
         # Electron fraction
-        self.ne = readsnapsgl(filename, "NE  ", quiet=True)
+        self.ne = readsnapsgl(self.filename, "NE  ", quiet=True)
         if self.ne is not 0:
             self.ne = self.ne[ids]
 
         # Temperature
-        self.temp = readsnapsgl(filename, "TEMP", quiet=True)
+        self.temp = readsnapsgl(self.filename, "TEMP", quiet=True)
         if self.temp is not 0:
             self.temp = self.temp[ids]
         else:
             raise ValueError("Can't get gas temperature, which is required for this code.")
 
         # density
-        self.rho = readsnapsgl(filename, "RHO ", quiet=True)
+        self.rho = readsnapsgl(self.filename, "RHO ", quiet=True)
         if self.rho is not 0:
             self.rho = self.rho[ids]
         else:
             raise ValueError("Can't get gas density, which is required")
 
         # smoothing length
-        self.hsml = readsnapsgl(filename, "HSML", quiet=True)
+        self.hsml = readsnapsgl(self.filename, "HSML", quiet=True)
         if self.hsml is not 0:
             self.hsml = self.hsml[ids]
 
         # mass only gas
-        self.mass = readsnapsgl(filename, "MASS", ptype=0, quiet=True)
+        self.mass = readsnapsgl(self.filename, "MASS", ptype=0, quiet=True)
         if self.mass is not 0:
             self.mass = self.mass[ids]
         else:
@@ -132,31 +145,14 @@ class load_data(object):
 
         # gas metal if there are
         if self.metal is 0:
-            self.metal = readsnapsgl(filename, "Z   ", ptype=0, quiet=True)  # auto calculate Z
+            self.metal = readsnapsgl(self.filename, "Z   ", ptype=0, quiet=True)  # auto calculate Z
             if self.metal is not 0:
                 self.metal = self.metal[ids]
 
-        # we need to remove some spurious particles.... if there is a MHI or SRF block
-        # see Klaus's doc or Borgani et al. 2003 for detials.
-        mhi = readsnapsgl(filename, "MHI ", quiet=True)
-        if mhi is 0:
-            # try exclude sfr gas particles
-            sfr = readsnapsgl(filename, "SFR ", quiet=True)
-            if sfr is not 0:
-                sfr = sfr[ids]
-                ids_ex = sfr < 0.1
-            else:
-                ids_ex = None
-        else:
-            mhi = mhi[ids] / 0.76 / self.mass
-            ids_ex = (self.temp < 3.0e4) & (self.rho > 6.e-7)
-            ids_ex = (mhi < 0.1) & (~ids_ex)
-            self.rho *= (1 - mhi)  # correct multi-phase baryon model by removing cold gas
-
         # Change NE (electron fraction to number density in simulation code/mp)
-        Zs = readsnapsgl(filename, "Zs  ", quiet=True)
+        Zs = readsnapsgl(self.filename, "Zs  ", quiet=True)
         if Zs is not 0:
-            self.ne *= self.rho * (1 - self.metal - Zs[:, 0])
+            self.ne *= self.rho * (1 - self.metal - Zs[ids, 0])
         else:
             if self.ne is not 0:
                 self.ne *= self.rho * (1 - self.metal - 0.24)  # assume constant Y = 0.24
@@ -164,7 +160,28 @@ class load_data(object):
                 # full ionized without taking metal into account
                 self.ne = 1.315789474 * self.rho * (1 - self.metal - 0.24)
 
-        if ids_ex is not None:
+        # we need to remove some spurious particles.... if there is a MHI or SRF block
+        # see Klaus's doc or Borgani et al. 2003 for detials.
+        mhi = readsnapsgl(self.filename, "MHI ", quiet=True)
+        if mhi is 0:
+            # try exclude sfr gas particles
+            sfr = readsnapsgl(self.filename, "SFR ", quiet=True)
+            if sfr is not 0:
+                sfr = sfr[ids]
+                ids_ex = sfr < 0.1
+                if sfr[ids_ex].size == sfr.size:
+                    ids_ex = True
+            else:
+                ids_ex = None
+        else:
+            mhi = mhi[ids] / 0.76 / self.mass
+            ids_ex = (self.temp < 3.0e4) & (self.rho > 6.e-7)
+            ids_ex = (mhi < 0.1) & (~ids_ex)
+            if mhi[ids_ex].size == mhi.size:
+                ids_ex = True
+            self.rho *= (1 - mhi)  # correct multi-phase baryon model by removing cold gas
+
+        if (ids_ex is not None) and (ids_ex is not True):
             self.temp = self.temp[ids_ex]     # cgs
             self.mass = self.mass[ids_ex]
             self.pos = self.pos[ids_ex]
@@ -177,7 +194,7 @@ class load_data(object):
             else:
                 self.hsml = (3*self.mass/self.pos/4/np.pi)**(1./3.)  # approximate
 
-    def _load_yt(self, filename, cc, rr, specified_field):
+    def _load_yt(self, specified_field):
         try:
             import yt
         except ImportError:
@@ -186,12 +203,12 @@ class load_data(object):
         if specified_field is not None:
             from yt.frontends.gadget.definitions import gadget_field_specs
             gadget_field_specs["my_def"] = specified_field
-            ds = yt.load(filename, field_spec="my_def")
+            ds = yt.load(self.filename, field_spec="my_def")
         else:
-            ds = yt.load(filename)
+            ds = yt.load(self.filename)
 
-        if (cc is not None) and (rr is not None):
-            sp = ds.sphere(center=cc, radius=(rr, "kpc/h"))
+        if (self.center is not None) and (self.radius is not None):
+            sp = ds.sphere(center=self.center, radius=(self.radius, "kpc/h"))
         else:
             sp = ds.all_data()
 
@@ -224,12 +241,12 @@ class load_data(object):
         # if ('Gas', 'SmoothingLength') in sp.ds.field_info.keys():
         #     self.hsml = sp[('Gas', 'SmoothingLength')].v
 
-    def _load_raw(self, datafile, cc, rr):
-        if (cc is not None) and (rr is not None):
-            r = np.sqrt(np.sum((datafile['pos'] - cc)**2, axis=1))
-            ids = r <= rr
-        else:
-            ids = np.ones(datafile['age'].size, dtype=bool)
-        self.temp = datafile['age'][ids]
-        self.mass = datafile['mass'][ids]
-        self.metal = datafile['metal'][ids]
+    # def _load_raw(self):
+    #     if (self.center is not None) and (self.radius is not None):
+    #         r = np.sqrt(np.sum((self.filename['pos'] - self.center)**2, axis=1))
+    #         ids = r <= self.radius
+    #     else:
+    #         ids = np.ones(self.filename['age'].size, dtype=bool)
+    #     self.temp = self.filename['age'][ids]
+    #     self.mass = self.filename['mass'][ids]
+    #     self.metal = self.filename['metal'][ids]
