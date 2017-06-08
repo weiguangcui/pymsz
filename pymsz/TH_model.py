a84f51a6f8455927ce071799f388be410fc814a6
weiguangcui <cuiweiguang@gmail.com>
2017-06-08 13:02:52 +0200

    using SPH smoothing function from rotate data

--- pymsz/TH_model.py
+++ pymsz/TH_model.py
@@ -1,5 +1,5 @@
 import numpy as np
-from rotate_data import rotate_data
+from rotate_data import rotate_data, SPH_smoothing
 from astropy.cosmology import FlatLambdaCDM, WMAP7
 # import pyfits
 # scipy must >= 0.17 to properly use this!
@@ -42,6 +42,8 @@ class TH_model(object):
                 Default : None, which gives npixel = 2 * cluster radius
                 and ignores the cluster's redshift.
                 Otherwise, cluster's redshift with AR decides how large the cluster looks.
+    SD       : dimensions for SPH smoothing. Type: int. Default: 2.
+                Must be 2 or 3!
     redshift : The redshift where the cluster is at.
                 Default : None, we will look it from simulation data.
                 If redshift = 0, it will be automatically put into 0.02,
@@ -64,14 +66,18 @@ class TH_model(object):
     mm=pymsz.TH_models(simudata, 1024, "z")
     """
 
-    def __init__(self, simudata, npixel=500, neighbours=None, axis='z', AR=None, redshift=None):
+    def __init__(self, simudata, npixel=500, neighbours=None, axis='z', AR=None, SD=2, redshift=None):
         self.npl = npixel
         self.ngb = neighbours
         self.ax = axis
         self.ar = AR
         self.red = redshift
         self.pxs = 0
+        self.SD = SD
         self.ydata = np.array([])
+
+        if self.SD not in [2, 3]:
+            raise ValueError("smoothing dimension must be 2 or 3" % SD)
 
         if simudata.data_type == "snapshot":
             self._cal_snap(simudata)
@@ -96,36 +102,28 @@ class TH_model(object):
                   (pos[:, 0] > -1 * simd.radius) & (pos[:, 0] <= simd.radius) & \
                   (pos[:, 1] > -1 * simd.radius) & (pos[:, 1] <= simd.radius)
             pos = pos[idc]
-        minx = pos[:, 0].min()
-        maxx = pos[:, 0].max()
-        miny = pos[:, 1].min()
-        maxy = pos[:, 1].max()
-        minz = pos[:, 2].min()
-        maxz = pos[:, 2].max()
         Tszdata = simd.Tszdata[idc]
-        if isinstance(simd.mass, type(0.0)):
-            mass = simd.mass
-        else:
-            mass = simd.mass[idc]
-        dens = simd.rho[idc]
 
         if isinstance(simd.hsml, type(0)):
             self.ngb = 27
+            hsml = None
         else:
             hsml = simd.hsml[idc]
+            self.ngb = None
 
-        # smearing the Tsz data using SPH with respected to the smoothing length
-        from scipy.spatial import cKDTree
         if self.ar is None:
+            minx = pos[:, 0].min()
+            maxx = pos[:, 0].max()
+            miny = pos[:, 1].min()
+            maxy = pos[:, 1].max()
+            minz = pos[:, 2].min()
+            maxz = pos[:, 2].max()
             self.pxs = np.min([maxx - minx, maxy - miny, maxz - minz]) / self.npl
-            # self.pxs *= (1 + 1.0e-6)
-            nx = np.int32(np.ceil((maxx - minx) / self.pxs)) + 1
-            ny = np.int32(np.ceil((maxy - miny) / self.pxs)) + 1
-            nz = np.int32(np.ceil((maxz - minz) / self.pxs)) + 1
-            self.ydata = np.zeros((nx, ny, nz), dtype=np.float32)
-            x = np.arange(minx, maxx, self.pxs)
-            y = np.arange(miny, maxy, self.pxs)
-            z = np.arange(minz, maxz, self.pxs)
+
+            if self.SD == 2:
+                self.ydata = SPH_smoothing(Tszdata, pos[:, :2], self.pxs, hsml=hsml, neighbors=self.ngb)
+            else:
+                self.ydata = SPH_smoothing(Tszdata, pos, self.pxs, hsml=hsml, neighbors=self.ngb)
         else:
             if self.red <= 0.0:
                 self.red = 0.02
@@ -135,52 +133,14 @@ class TH_model(object):
             else:
                 cosmo = WMAP7
             self.pxs = cosmo.arcsec_per_kpc_proper(self.red) * self.ar * simd.cosmology['h']
-            self.ydata = np.zeros((self.npl, self.npl, self.npl), dtype=np.float32)
-            x = np.arange(minx, maxx, self.pxs)
-            y = np.arange(miny, maxy, self.pxs)
-            z = np.arange(minz, maxz, self.pxs)
-
-        # if self.ngb is not None:
-        #     hsml = np.sqrt(self.ngb) * self.pxs
-        x, y, z = np.meshgrid(x, y, z, indexing='ij')
-        xyz = np.concatenate((x.reshape(x.size, 1), y.reshape(
-            y.size, 1), z.reshape(z.size, 1)), axis=1)
-        mtree = cKDTree(pos)
-        if self.ngb is not None:
-            dist, idst = mtree.query(xyz, self.ngb)
-        for i in np.arange(xyz.shape[0]):
-            if self.ngb is not None:
-                ids = idst[i]
-                # mxhsml = dist[i].max()
-                ihsml = hsml[ids]
-                # ihsml[ihsml < mxhsml] = mxhsml
-                ihsml3 = 1. / ihsml**3
-                if isinstance(mass, type(0.0)):
-                    wsph = (mass / dens[ids]) * ihsml3
-                else:
-                    wsph = (mass[ids] / dens[ids]) * ihsml3
-                wsph *= SPH(dist[i] / ihsml)
+            if self.SD == 2:
+                self.ydata = SPH_smoothing(Tszdata, pos[:, :2], self.pxs, hsml=hsml,
+                                           neighbors=self.ngb, pxln=self.npl)
             else:
-                ihsml3 = 1. / hsml[i]**3
-                ids = mtree.query_ball_point(pos[i], hsml[i])
-                # if isinstance(ids, type(0)):  # int object
-                #     ids = np.array([ids])
-                if len(ids) == 0:
-                    wsph = np.array([1])
-                    dist, ids = mtree.query(pos[i], k=1)
-                else:
-                    dist = np.sqrt((pos[i, 0] - mtree.data[ids, 0]) **
-                                   2 + (pos[i, 1] - mtree.data[ids, 1])**2)
-                    wsph = SPH(dist / hsml[i]) * ihsml3
-            # xx = np.int32((mtree.data[ids, 0] - minx) / self.pxs)
-            # yy = np.int32((mtree.data[ids, 1] - miny) / self.pxs)
-            # zz = np.int32((mtree.data[ids, 2] - minz) / self.pxs)
-            xx = np.int32((xyz[i, 0] - minx) / self.pxs)
-            yy = np.int32((xyz[i, 1] - miny) / self.pxs)
-            zz = np.int32((xyz[i, 2] - minz) / self.pxs)
-            self.ydata[xx, yy, zz] += np.sum(Tszdata[ids] * wsph) / wsph.sum()
+                self.ydata = SPH_smoothing(Tszdata, pos, self.pxs, hsml=hsml,
+                                           neighbors=self.ngb,  pxln=self.npl)
 
-        self.ydata = np.sum(self.ydata, axis=2) * self.pxs * Kpc / simd.cosmology["h"]
+        self.ydata *= self.pxs * Kpc / simd.cosmology["h"]
 
     def _cal_yt(self, simd):
         # from yt.units import cm
