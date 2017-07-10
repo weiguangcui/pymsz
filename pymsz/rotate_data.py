correct parallel calculation

pymsz/rotate_data.py
@@ -178,7 +178,7 @@ def sph_kernel_wendland6(x):
     return kernel
 
 
-def cal_sph_2d(n, mtree, pos, hsml, indxyz, sphkernel, wdata, ydata):
+def cal_sph_2d(n, mtree, pos, hsml, pxln, indxyz, sphkernel, wdata, ydata):
     if np.max(n) >= pos.shape[0]:
         n = n[n < pos.shape[0]]
     for i in n:
@@ -186,10 +186,10 @@ def cal_sph_2d(n, mtree, pos, hsml, indxyz, sphkernel, wdata, ydata):
         if len(ids) != 0:
             dist = np.sqrt(np.sum((pos[i] - mtree.data[ids])**2, axis=1))
             wsph = sphkernel(dist/hsml[i])
-            ydata[indxyz[ids, 0], indxyz[ids, 1]] += wdata[i] * wsph / wsph.sum()
+            ydata[indxyz[ids, 0]*pxln+indxyz[ids, 1]] += wdata[i] * wsph / wsph.sum()
         else:
             dist, ids = mtree.query(pos[i], k=1)
-            ydata[indxyz[ids, 0], indxyz[ids, 1]] += wdata[i]
+            ydata[indxyz[ids, 0]*pxln+indxyz[ids, 1]] += wdata[i]
 
 
 def SPH_smoothing(wdata, pos, pxls, hsml=None, neighbors=64, pxln=None,
@@ -361,7 +361,7 @@ def SPH_smoothing(wdata, pos, pxls, hsml=None, neighbors=64, pxln=None,
                 N = np.int32(np.ceil(pos.shape[0]/cpu_count()))
                 for i in range(0, cpu_count()):
                     p = Process(target=cal_sph_2d, args=(range(i*N, (i+1)*N), mtree, pos, hsml,
-                                                         indxyz, sphkernel, wdata, ydata))
+                                                         pxln, indxyz, sphkernel, wdata, ydata))
                     p.start()
                 p.join()
                 # for i in np.arange(pos.shape[0]):