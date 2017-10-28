0ec3fa31619db629d54e479823f12c37e7014cf2
Liu Yufang <yaya@Lius-MacBook-Pro.local>
2017-10-28 17:03:42 +0200

    Cutting to data from spherical ball to cubic box

--- pymsz/load_data.py
+++ pymsz/load_data.py
@@ -166,8 +166,12 @@ class load_data(object):
         # positions # only gas particles
         spos = readsnapsgl(self.filename, "POS ", ptype=0, quiet=True)
         if (self.center is not None) and (self.radius is not None):
-            r = np.sqrt(np.sum((spos - self.center)**2, axis=1))
-            ids = r <= self.radius  # increase to get all the projected data
+            # r = np.sqrt(np.sum((spos - self.center)**2, axis=1))
+            # ids = r <= self.radius  # increase to get all the projected data
+            # Now using cubic box to get the data
+            ids = (spos[:,0]>=self.center[0]-self.radius)&(spos[:,0]<=self.center[0]+self.radius) &
+                  (spos[:,1]>=self.center[1]-self.radius)&(spos[:,1]<=self.center[1]+self.radius) &
+                  (spos[:,2]>=self.center[2]-self.radius)&(spos[:,2]<=self.center[2]+self.radius)
             self.pos = spos[ids] - self.center
         else:
             ids = np.ones(head[0][0], dtype=bool)
