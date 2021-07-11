bug fix in temperature calculation and minor updates

pymsz/load_data.py
@@ -261,33 +261,39 @@ class load_data(object):
                 self.X = 1 - self.metal[ids,0] - self.metal[ids,1] # Now self.X is hydrogen mass fraction, electron number = M*X/m_H*NE
                 self.metal = self.metal[ids,0]
                 
-        # Temperature
-        U = readsnap(self.filename, "InternalEnergy", quiet=True, ptype=0)
-        if U is None:
-            raise ValueError('InternalEnergy for calculating temperature can not be read from simulation!')
-        else:
-            U = U[ids]
-            # U = sn['/PartType0/InternalEnergy'][ids]
-            v_unit = 1.0e5 * np.sqrt(self.cosmology["a"])       # (e.g. 1.0 km/sec)
-            prtn = 1.67373522381e-24  # (proton mass in g)
-            bk = 1.3806488e-16        # (Boltzman constant in CGS)
-            # xH = 0.76  # hydrogen mass-fraction
-            yhelium = (1. - self.X) / (4 * self.X)
-            self.ne = readsnap(self.filename, "ElectronAbundance", quiet=True, ptype=0)
+        # ne 
+        yhelium = (1. - self.X) / (4 * self.X)
+        self.ne = readsnap(self.filename, "ElectronAbundance", quiet=True, ptype=0)
+        if self.ne is not None:
+            self.ne = self.ne[ids]
+        if self.mu is None:
             if self.ne is not None:
-                self.ne = self.ne[ids]
+                self.mu = (1. + 4. * yhelium) / (1. + yhelium + self.ne)
+            else:
+                self.mu = (1. + 4. * yhelium) / (1. + 3 * yhelium + 1)  # assume full ionized
+                self.ne = np.ones(self.rho.size) * (4.0 / self.mu - 3.28) / 3.04
+        else:  #Everything will be set by mu
+            if self.ne is None:
+                self.ne = np.ones(self.rho.size) * (4.0 / self.mu - 3.28) / 3.04
                 
-            if self.mu is None:
-                if self.ne is not None:
-                    self.mu = (1. + 4. * yhelium) / (1. + yhelium + self.ne)
-                else:
-                    self.mu = (1. + 4. * yhelium) / (1. + 3 * yhelium + 1)  # assume full ionized
-                    self.ne = np.ones(self.rho.size) * (4.0 / self.mu - 3.28) / 3.04
-            else:  #Everything will be set by mu
-                if self.ne is None:
-                    self.ne = np.ones(self.rho.size) * (4.0 / self.mu - 3.28) / 3.04
-            self.temp = U * (5. / 3 - 1) * v_unit**2 * prtn * self.mu / bk
-            U = 0
+        # Temperature
+        temp = readsnap(self.filename, "Temperature", mu=self.mu, quiet=True, ptype=0)
+        if temp is not None:
+            self.temp = temp
+            temp = 0
+        else:
+            U = readsnap(self.filename, "InternalEnergy", quiet=True, ptype=0)
+            if U is None:
+                raise ValueError('InternalEnergy for calculating temperature can not be read from simulation!')
+            else:
+                U = U[ids]
+                # U = sn['/PartType0/InternalEnergy'][ids]
+                v_unit = 1.0e5      # (e.g. 1.0 km/sec)
+                prtn = 1.67373522381e-24  # (proton mass in g)
+                bk = 1.3806488e-16        # (Boltzman constant in CGS)
+                # xH = 0.76  # hydrogen mass-fraction
+                self.temp = U * (5. / 3 - 1) * v_unit**2 * prtn * self.mu / bk
+                U = 0
         
         # density
         self.rho = readsnap(self.filename, "Density", quiet=True, ptype=0)
@@ -395,9 +401,47 @@ class load_data(object):
                 self.bulkvel = np.mean(self.vel[r < self.hmrad], axis=0)
                 self.vel -= self.bulkvel
 
+        # gas metal if there are
+        if self.metal is None:
+            self.metal = readsnap(self.filename, "Z   ", ptype=0, nmet=self.Nmets,
+                                     quiet=True)  # auto calculate Z
+            if self.metal is not None:
+                self.metal = self.metal[ids]
+            else:
+                raise ValueError("No metallicity! please give me a value as I can't find in snapshot!")
+
+        Zs = readsnap(self.filename, "Zs  ", ptype=0, quiet=True)
+        if Zs is not None:
+            self.X = 1 - self.metal - Zs[:, 0][ids]/self.mass  # hydrogen mass fraction assume Tornatore et al. 2007.
+        else:
+            self.X = 1 - self.metal - 0.24  # simply assume He=0.24
+            # Now self.X is hydrogen mass fraction, electron number = M*X/m_H*NE
+        # else:  # for no matalicity!
+        #     # Change NE (electron number fraction respected to H number density in simulation)
+        #     # M/mmw/mp gives the total nH+nHe+ne! To get the ne, which = self.ne*nH, we reexpress this as ne*Q_NE.
+        #     # Q_NE is given in self.ne in below.
+        #     self.ne = (1. + yhelium + self.ne) / self.ne
+        
+        # Electron fraction
+        self.ne = readsnap(self.filename, "NE  ", quiet=True)
+        # ( 1. - xH ) / ( 4 * xH )hydrogen mass-fraction (xH) = n_He/n_H
+        # yhelium = 0.07894736842105263
+        yhelium = (1. - self.X) / (4 * self.X)
+        if self.ne is not None:
+            self.ne = self.ne[ids]
+            self.mmw = (1. + 4. * yhelium) / (1. + yhelium + self.ne)
+        else:  # calculate NE from mean mol weight
+            self.mmw = (1. + 4. * yhelium) / (1. + 3 * yhelium + 1)  # assume full ionized
+            if self.mu is None:
+                # full ionized without taking metal into account. What about metal?
+                self.ne = np.ones(self.rho.size) * (4.0 / self.mmw - 3.28) / 3.04
+                # (4.0 / self.mmw - 3.0 * 0.76 - 1.0) / 4.0 / 0.76
+            else:
+                self.ne = np.ones(self.rho.size) * (4.0 / self.mu - 3.28) / 3.04
+
         # Temperature
         if self.mu is None:
-            self.temp = readsnap(self.filename, "TEMP", quiet=True)
+            self.temp = readsnap(self.filename, "TEMP", my=self.mmw, quiet=True)
         else:
             self.temp = readsnap(self.filename, "TEMP", mu=self.mu, quiet=True)
         if self.temp is not None:
@@ -421,41 +465,6 @@ class load_data(object):
         self.mass = readsnap(self.filename, "MASS", ptype=0, quiet=True)
         if not isinstance(self.mass, type(0.0)):
             self.mass = self.mass[ids]
-
-        # gas metal if there are
-        if self.metal is None:
-            self.metal = readsnap(self.filename, "Z   ", ptype=0, nmet=self.Nmets,
-                                     quiet=True)  # auto calculate Z
-            if self.metal is not None:
-                self.metal = self.metal[ids]
-
-        # Electron fraction
-        self.ne = readsnap(self.filename, "NE  ", quiet=True)
-        # ( 1. - xH ) / ( 4 * xH )hydrogen mass-fraction (xH) = n_He/n_H
-        yhelium = 0.07894736842105263
-        if self.ne is not None:
-            self.ne = self.ne[ids]
-            self.mmw = (1. + 4. * yhelium) / (1. + yhelium + self.ne)
-        else:  # calculate NE from mean mol weight
-            self.mmw = (1. + 4. * yhelium) / (1. + 3 * yhelium + 1)  # assume full ionized
-            if self.mu is None:
-                # full ionized without taking metal into account. What about metal?
-                self.ne = np.ones(self.rho.size) * (4.0 / self.mmw - 3.28) / 3.04
-                # (4.0 / self.mmw - 3.0 * 0.76 - 1.0) / 4.0 / 0.76
-            else:
-                self.ne = np.ones(self.rho.size) * (4.0 / self.mu - 3.28) / 3.04
-
-        Zs = readsnap(self.filename, "Zs  ", ptype=0, quiet=True)
-        if Zs is not None:
-            self.X = 1 - self.metal - Zs[:, 0][ids]/self.mass  # hydrogen mass fraction assume Tornatore et al. 2007.
-        else:
-            self.X = 1 - self.metal - 0.24  # simply assume He=0.24
-            # Now self.X is hydrogen mass fraction, electron number = M*X/m_H*NE
-        # else:  # for no matalicity!
-        #     # Change NE (electron number fraction respected to H number density in simulation)
-        #     # M/mmw/mp gives the total nH+nHe+ne! To get the ne, which = self.ne*nH, we reexpress this as ne*Q_NE.
-        #     # Q_NE is given in self.ne in below.
-        #     self.ne = (1. + yhelium + self.ne) / self.ne
 
         # we need to remove some spurious particles.... if there is a MHI or SRF block
         # see Klaus's doc or Borgani et al. 2003 for detials.

pymsz/readsnapsgl.py
@@ -216,7 +216,7 @@ def readsnapsgl(filename, block, endian=None, quiet=False, longid=False, nmet=11
                             mean_mol_weight = mu
                     else:
                         mean_mol_weight = (1. + 4. * yhelium) / (1. + yhelium + NE)
-                    v_unit = 1.0e5 * np.sqrt(hd.Time)       # (e.g. 1.0 km/sec)
+                    v_unit = 1.0e5      # (e.g. 1.0 km/sec)
                     prtn = 1.67373522381e-24  # (proton mass in g)
                     bk = 1.3806488e-16        # (Boltzman constant in CGS)
                     npf.close()
@@ -591,10 +591,37 @@ def readhdf5head(filename, quiet=False):
     fo.close()
     return hd
 
-def readhdf5data(filename, block, quiet=False, ptype=None):
+def readhdf5data(filename, block, mu=mu, quiet=False, ptype=None):
     if not quiet:
         print('Reading file ', filename, ' with data block ', block,' for type ', ptype)
     fo = h5py.File(filename, 'r')
+    
+    if (block.lower() == 'temperature') & ('temperature' not in [k.lower() for k in fo['PartType0'].keys()]):
+        if 'InternalEnergy' not in fo['PartType0'].keys():
+            print("Can't read gas Temperature and internal energy as both are not in gas properties: !!", fo['PartType0'].keys())
+            fo.close()
+            return None
+        else:
+            temp = fo['PartType0/InternalEnergy'][:]
+            if 'Metallicity' not in fo['PartType0'].keys(): 
+                xH = 0.76  # hydrogen mass-fraction
+            else:
+                Metals=fo['PartType0/Metallicity'][:]
+                xH = 1-Metals[:,0]-Metals[:,1]
+            yhelium = (1. - xH) / (4 * xH)
+            if mu is not None:
+                mean_mol_weight = mu
+            else:
+                if 'ElectronAbundance' not in fo['PartType0'].keys(): # we assume it is NR run with full ionized gas n_e/nH = 1 + 2*nHe/nH
+                    mean_mol_weight = (1. + 4. * yhelium) / (1. + 3 * yhelium + 1)
+                else:
+                    NE = fo['PartType0/ElectronAbundance'][:]
+                    mean_mol_weight = (1. + 4. * yhelium) / (1. + yhelium + NE)
+            v_unit = 1.0e5      # (e.g. 1.0 km/sec)
+            prtn = 1.67373522381e-24  # (proton mass in g)
+            bk = 1.3806488e-16        # (Boltzman constant in CGS)
+            fo.close()
+            return(temp * (5. / 3 - 1) * v_unit**2 * prtn * mean_mol_weight / bk)
     
     if isinstance(ptype, type(0)):
         if 'PartType'+str(ptype) in fo.keys():
@@ -679,7 +706,7 @@ def readsnap(filename, block, endian=None, quiet=False, longid=False, nmet=11,
     else:
         filename=glob(filename+'*')
         filenum=len(filename) 
-        if (len(filename) > 1) and ('hdf5' in ','.join(filename).lower()):
+        if (len(filename) > 1) and ('.hdf5' in ','.join(filename).lower()):
             filename = [ x for x in filename if x[-4:].lower() == 'hdf5']  #exclude the other files      
             filenum=len(filename)        
         else:
@@ -692,6 +719,13 @@ def readsnap(filename, block, endian=None, quiet=False, longid=False, nmet=11,
         head = readhdf5head(filename[0], quiet=quiet)
         if block == 'Header':
             return head
+        elif (block == 'IDTP') or (block == 'IDTypes'):
+            data=np.zeros(head.npart[0],dtype=np.int32)
+            for i in np.arange(1,6):
+                if head.npart[i]>0:
+                    data = np.append(data, np.ones(head.npart[i],dtype=np.int32)*i, axis=0)
+            return data
+            
     else:
         head=readsnapsgl(filename[0], 'HEAD', endian=endian, quiet=quiet, longid=longid, nmet=nmet,
                          fullmass=fullmass, mu=mu, fmt=fmt, ptype=ptype, rawdata=rawdata)
@@ -703,7 +737,7 @@ def readsnap(filename, block, endian=None, quiet=False, longid=False, nmet=11,
         
     if head.Numfiles == 1: # only one file
         if filename[0][-4:].lower() == 'hdf5':
-            return readhdf5data(filename[0], block, quiet=quiet, ptype=ptype)
+            return readhdf5data(filename[0], block, mu=mu, quiet=quiet, ptype=ptype)
         else:
             return readsnapsgl(filename[0], block, endian=endian, quiet=quiet, longid=longid, nmet=nmet,
                                fullmass=fullmass, mu=mu, fmt=fmt, ptype=ptype, rawdata=rawdata)
@@ -711,13 +745,13 @@ def readsnap(filename, block, endian=None, quiet=False, longid=False, nmet=11,
         for i,fbase in enumerate(filename):
             if i == 0:
                 if fbase[-4:].lower() == 'hdf5':
-                    data = readhdf5data(fbase, block, quiet=quiet, ptype=ptype)
+                    data = readhdf5data(fbase, block, mu=mu, quiet=quiet, ptype=ptype)
                 else:
                     data = readsnapsgl(fbase, block, endian=endian, quiet=quiet, longid=longid, nmet=nmet,
                                        fullmass=fullmass, mu=mu, fmt=fmt, ptype=ptype, rawdata=rawdata)
             else:
                 if fbase[-4:].lower() == 'hdf5':
-                    tmp = readhdf5data(fbase, block, quiet=quiet, ptype=ptype)
+                    tmp = readhdf5data(fbase, block, mu=mu, quiet=quiet, ptype=ptype)
                 else:
                     tmp = readsnapsgl(fbase, block, endian=endian, quiet=quiet, longid=longid, nmet=nmet,
                                       fullmass=fullmass, mu=mu, fmt=fmt, ptype=ptype, rawdata=rawdata)